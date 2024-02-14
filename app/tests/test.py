00d8726bdd2211ae515958e29941a1fa5aa853c5
Mahmood <mahmood.lotfalizade@gmail.com>
2024-02-14 16:57:02 +0100

    Check

--- app/tests/test.py
+++ app/tests/test.py
@@ -3,4 +3,6 @@ import yfinance as yf
 # stock = yf.Ticker("V")
 # info = stock.info
 # print(list(info.keys()))
-print(yf.Ticker("GOOG").fast_info)
+# print(yf.Ticker("GOOG").fast_info)
+
+print(yf.Ticker("GOOG").info.get("symbol", "KIRekhar"))
